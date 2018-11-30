14f61eacc5f4d37f6a0b5426dc47f7003038083b
Michael Banzon <mrnbanzon@gmail.com>
2018-11-29 22:10:41 -0800

    Maintain state via redux.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducers/index';
 import App from './components/App';
 
-render(<App />, document.getElementById('app'));
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
